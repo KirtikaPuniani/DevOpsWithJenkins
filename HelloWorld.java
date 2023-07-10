f6083caa2ebd1aadec207e795a514ecb99f02746
Kirtika Puniani <75201661+KirtikaPuniani@users.noreply.github.com>
2023-07-11 00:01:45 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+adbddddd
+=======
+public class hello{
+    public static void main(String[] args){
+        system.out.println("Hello hello wwwWwworld");
+    }
+}
+>>>>>>> aea73782e79bb38cfaec3cacc699b793abffb52d
